2b8d3ec7a60fbd862f842c171d7795f70f0904e9
DimaChere <124854694+DimaChere@users.noreply.github.com>
2023-08-31 02:25:46 +0600

    Create testscript.js

---
+++ testscript.js
@@ -0,0 +1,13 @@
+"use strict";
+let dots = document.getElementsByClassName("script-part__dot");
+async function animateDots() {
+  let i = 0;
+  let int = setInterval(() => {
+    if (i >= dots.length - 1) {
+      clearInterval(int);
+    }
+    dots[i].style.backgroundColor = "rgb(245 222 25)";
+    i++;
+  }, 200);
+  i = 0;
+}
